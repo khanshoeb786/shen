93c645edda2250791357c15598db6a5287247574
khanshoeb786 <31188011+khanshoeb786@users.noreply.github.com>
2017-08-20 22:26:53 +0530

    dfg
    dfh

---
+++ den_formdt.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs= require('fs');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(express.static('public'));
+app.get('/button.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "button.htm" );
+})
+
+app.post('/download', urlencodedParser,function(req, res){
+
+response = {
+      first_name:req.body.first_name,
+      last_name:req.body.last_name
+   };
+response=JSON.stringify(response)
+ console.log(response);
+  var file = __dirname + "/"+'bc.txt';
+fs.writeFile(file , response, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+}); 
+  var filename ="dooo.txt";
+
+  res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+
+  var filestream = fs.createReadStream(file);
+  filestream.pipe(res);
+});
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+
+})
